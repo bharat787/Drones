7d0ee7f6e0c9db00311c3ce20506aac0e0d430a8
bharat787 <bgbharat787@gmail.com>
2019-05-19 18:29:49 +0530

    added a comment

--- arm.py
+++ arm.py
@@ -1,4 +1,4 @@
-#/usr/bin/env python
+#/usr/bin/env python #this a comment
 # -*- coding: utf-8 -*-
 from dronekit import connect,VehicleMode,LocationGlobalRelative,Vehicle
 from pymavlink import mavutil
